cb191c58458ed8a0ad42b81cdef9b02cd14ebbe1
milosz sitarski <miloszgamer2@onet.pl>
2019-12-17 09:34:11 +0100

    until,select

---
+++ bash/z6.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#pętla until
+
+
+i=10
+
+until [ ${i} -eq 10 ]; do
+    echo ${i}
+    i=$[i+1]
+done
+
+
+
+#petla select
+echo "Podaj opcję:"
+
+select opcja in File Edit Quit
+do
+    case ${opcja} in
+	"File") echo "Wybrano opcję ${opcja}" ;;
+	"Edit") echo "Wybrano opcję ${opcja}" ;;
+	"Quit") exit ;;
+	*) echo "Podaj liczby z zakresu [1..3]" ;;
+    esac
+done
+
