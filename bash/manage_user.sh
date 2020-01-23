dd168dd72bd9e9ecddb33a808b00b77889c1bae8
Your Name <you@example.com>
2020-01-23 09:25:58 +0100

    printf, color, debugowanie

--- bash/manage_user.sh
+++ bash/manage_user.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
+
 #zmienna globalne
 #ładowanie danych z pliku
 user_list=(`cat users.txt`)
 status="FAIL"
-RED='E[31m'
-GREEN=
-NC=
-
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\e[0m'
 
 function checkStatusCode() {
     if [ $? == 0 ]; then
-	status="PASS"
+	status="${GREEN}[PASS]${NC}"
     else
-	status="FAIL"
+	status="${RED}[FAIL]${NC}"
     fi
 }
 
@@ -36,7 +36,8 @@ function addUsers () {
     do
 	sudo useradd ${user} -m -s /sbin/nologin -g "users" 2> /dev/null
 	    checkStatusCode
-	    echo "Add user: ${user} [${status}]"
+	    #echo -e "Add user: ${user} [${status}]"
+	    printf "Add user %11b %21b\n" "${user}" "${status}"
 	done
     fi
 }
@@ -51,7 +52,8 @@ function delUsers () {
 	do
 	    sudo userdel -r ${user} 2> /dev/null
 	    checkStatusCode
-	    echo "Remove user ${user} [${status}]"
+	    #echo "Remove user ${user} [${status}]"
+	    printf "Remove user %11b %21b\n" "${user}" "${status}"
 	done
     fi
 }
@@ -64,8 +66,8 @@ function acceptRemoteLogin () {
     do
 	sudo usermod -s /bin/bash ${user} 2> /dev/null
 	checkStatusCode
-        echo "Accept remote login for ${user} [${status}]"
-    
+        #echo "Accept remote login for ${user} [${status}]"
+	printf "Accept remote login for %11b %21b\n" "${user}" "${status}"
     done
 }
 
@@ -77,7 +79,8 @@ function deniedRemoteLogin () {
         
         sudo usermod -s /sbin/nologin ${user} 2> /dev/null
 	checkStatusCode
-	echo "Denied remote login for ${user} [${status}]"
+	#echo "Denied remote login for ${user} [${status}]"
+	printf "Denied remote login for %11b %21b\n" "${user}" "${status}"
     done
 }
 function quit () {
